61690d3979f76bf8e08ead53e5fb3433b3d362d2
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-06-03 20:42:20 -0400

    Projeto Finalizado
    Utilizando localstorege como bd

--- Controle de Gastos/recursos/js/app.js
+++ Controle de Gastos/recursos/js/app.js
@@ -38,8 +38,51 @@ class BD{
         localStorage.setItem(id,JSON.stringify(d))
         localStorage.setItem('id',id)
     }
-    
 
+    recuperarTodosRegistros(){
+        let despesas  = Array()
+
+        let id = localStorage.getItem('id')
+
+        for(let i = 1;i<=id;i++){
+            let despesa =  JSON.parse(localStorage.getItem(i))
+
+            if(despesa === null){
+                continue
+            }
+            despesa.id = i
+            despesas.push(despesa)
+        }
+
+        return despesas
+    }
+
+    pesquisar(despesa) {
+
+        let despesasFiltradas =  Array()
+        despesasFiltradas = this.recuperarTodosRegistros()
+
+        if(despesa.data != ""){
+            despesasFiltradas =despesasFiltradas.filter(function(d){ return d.data == despesa.data})
+        }
+        if(despesa.tipo_gastos != ""){
+            despesasFiltradas= despesasFiltradas.filter(d =>  d.tipo_gastos == despesa.tipo_gastos)
+        }
+        
+        if(despesa.valor != ""){
+            despesasFiltradas= despesasFiltradas.filter(d =>  d.valor == despesa.valor)
+        }
+
+        return despesasFiltradas
+        
+        
+
+   }  
+
+   remover(id){
+        localStorage.removeItem(id)
+   }
+    
 }
 
 let bd = new BD()
@@ -60,7 +103,6 @@ function novasDespesas(){
                                 descricao.value,
                                 valor.value)
 
-
         if(despesas.dadosValidos()){        
             fade.classList.toggle('hide')
             modal.classList.toggle('hide')
@@ -69,7 +111,13 @@ function novasDespesas(){
             aviso.textContent = "Dados Salvos com Sucesso"
             button.textContent = "Fechar"
             console.log("dados validos")
-           // bd.gravar(despesas)
+            bd.gravar(despesas)
+
+            data.value = " " 
+            tipo_gastos.value = " "
+            descricao.value = " "
+            valor.value = " "  
+
         }else{
             fade.classList.toggle('hide')
             modal.classList.toggle('hide')
@@ -78,7 +126,66 @@ function novasDespesas(){
             aviso.textContent = "Existem campos que não foram preenchidos, por favor preenche-los e enviar novamente."
             button.textContent = "Fechar e Corrigir"
             console.log("dados invalidos")
-        }
+        }        
+}
+
+function carregarListaDespesas(despesas = Array(), filter = false){
+  
+    if(despesas.length == 0 && filter == false){
+        despesas = bd.recuperarTodosRegistros()
+    }
+     
+     let listaDespesa = document.getElementById("lista_despesas")
+     listaDespesa.innerHTML =''
+
+     despesas.forEach(function(d){
+
+      let linha =  listaDespesa.insertRow()
+       
+      linha.insertCell(0).innerHTML = d.data
+     
+
+      switch(d.tipo_gastos){
+            case '1' :  d.tipo_gastos = 'Energia Elétrica'
+                 break
+            case '2' : d.tipo_gastos = 'Transporte'
+                 break
+            case '3' : d.tipo_gastos = 'Educação'
+                 break
+
+      }
+      linha.insertCell(1).innerHTML = d.tipo_gastos
+      linha.insertCell(2).innerHTML = d.descricao
+      linha.insertCell(3).innerHTML = d.valor
+      let btn = document.createElement('button')
+      btn.className = 'excluir'
+      btn.innerHTML =  '<i class="fas fa-times"></i>'
+      btn.id = 'id_despesa_'+d.id
+      btn.onclick = function(){
+           let id= this.id.replace('id_despesa_','')
+            bd.remover(id)
+
+            window.location.reload()
+      }
+      linha.insertCell(4).append(btn)
+
+
+
+     })
+}
+
+
+function pesquisarDespesas(){
+    let data =  document.getElementById('data').value
+    let tipo_gastos =  document.getElementById('tipo_gasto').value
+    let valor =   document.getElementById('valor').value
+
+
+    let despesa = new Despesa(data,tipo_gastos,null,valor)
+
+    let despesas = bd.pesquisar(despesa)
+
+    carregarListaDespesas(despesas, true)
 }
 
  function fecharModal(){
